a4eb3e625da9833627ec65964a7bf0e7b8ce1c5a
ashumehta18 <your-email@example.com>
2026-09-09 14:08:21 +0530

    deployment urls corrected

--- backend/src/app.js
+++ backend/src/app.js
@@ -34,7 +34,7 @@ app.use(helmet());
 
 app.use(cors({
   origin: [
-    'https://campus-iq-zeta.vercel.app/login',
+    'https://campus-iq-zeta.vercel.app',
     'http://localhost:5173' // for local development
   ],
   credentials: true
